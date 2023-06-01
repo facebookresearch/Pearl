b76661973c6210c0bf27c57b14fefe93a5980070
Hongbo Guo <hongbog@meta.com>
2023-06-01 14:05:05 -0700

    add basic uncertainty unit test
    Summary: This validates that the migrated pear-LinUCB also outputs uncertainty fine (besides outputs reward fine)
    
    Differential Revision: D46344261
    
    fbshipit-source-id: e74c4f83a9e03ab7584ae579aeb7b102a352be90

--- pearl/test/unittest/with_pytorch/test_linucb.py
+++ pearl/test/unittest/with_pytorch/test_linucb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env fbpython
 # (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+import logging
 import unittest
 
 import torch
@@ -8,6 +9,8 @@ from pearl.contextual_bandits.linUCB import LinUCB
 from pearl.policy_learners.exploration_module.no_exploration import NoExploration
 from pearl.replay_buffer.transition import TransitionBatch
 from pearl.utils.action_spaces import DiscreteActionSpace
+
+logger = logging.getLogger(__name__)
 
 
 class TestLinUCB(unittest.TestCase):
@@ -95,3 +98,78 @@ class TestLinUCB(unittest.TestCase):
                 atol=1e-4,
             )
         )
+
+    def test_lin_ucb_uncertainty(self) -> None:
+        """
+        Since A is init with zeros, the uncertainty should be proportional to number of obs
+        """
+
+        policy_learner = LinUCB(
+            feature_dim=2,
+            exploration_module=NoExploration(),
+        )
+
+        # 1st arm = [1, 0]
+        # 2nd arm = [0, 1]
+        batch = TransitionBatch(
+            state=torch.tensor(
+                [
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],
+                    [1.0],  # repeat 1st arm ten times
+                    [0.0],
+                ]
+            ),
+            action=torch.tensor(
+                [
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [0.0],
+                    [1.0],
+                ]
+            ),
+            reward=torch.tensor(
+                [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 4.0]
+            ),
+            weight=torch.tensor(
+                [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+            ),
+        )
+
+        policy_learner.learn_batch(batch)
+        _ = policy_learner._linear_regression(
+            torch.cat([batch.state[0], batch.action[0]])
+        )  # predict reward
+
+        # test uncertainty of policy_learner (LinUCB)
+        A = policy_learner._linear_regression._A
+        A_inv = torch.linalg.inv(A)
+        x = torch.cat([batch.state, batch.action], dim=1)
+        uncertainty = torch.diagonal(x @ A_inv @ x.t())
+        logger.info(uncertainty)
+
+        # the 2nd arm's uncertainty is 10 times 1st arm's uncertainty
+        uncertainty_ratio = torch.tensor(uncertainty[-1] / uncertainty[0])
+        self.assertTrue(
+            torch.allclose(
+                uncertainty_ratio,
+                torch.tensor(10.0),  # the 1st arm occured 10 times than 2nd arm
+                rtol=0.01,
+            )
+        )
+
+        return
