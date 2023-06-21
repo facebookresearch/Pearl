ed7b37a6a686f72f11f339752efefa8d0f85f3b1
Zheqing (Bill) Zhu <billzhu@meta.com>
2023-06-20 18:18:10 -0700

    On-policy Episodic Return Replay Buffer
    Summary: On-policy Episodic Return Replay Buffer
    
    Differential Revision: D46586800
    
    fbshipit-source-id: 5b47599ec2b6aaab95ba1b079f4b7dcba5333241

--- pearl/replay_buffer/transition.py
+++ pearl/replay_buffer/transition.py
@@ -8,7 +8,7 @@ Transition is designed for one single set of data
 """
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=False)
 class Transition:
     state: torch.tensor
     action: torch.tensor
@@ -17,8 +17,8 @@ class Transition:
     next_action: Optional[torch.tensor] = None
     curr_available_actions: torch.tensor = None
     curr_available_actions_mask: torch.tensor = None
-    next_available_actions: torch.tensor = None
-    next_available_actions_mask: torch.tensor = None
+    next_available_actions: Optional[torch.tensor] = None
+    next_available_actions_mask: Optional[torch.tensor] = None
     done: torch.tensor = None
     weight: torch.tensor = None
 
@@ -33,11 +33,11 @@ class TransitionBatch:
     state: torch.tensor
     action: torch.tensor
     reward: torch.tensor
-    next_state: torch.tensor = None
+    next_state: Optional[torch.tensor] = None
     next_action: Optional[torch.tensor] = None
     curr_available_actions: torch.tensor = None
     curr_available_actions_mask: torch.tensor = None
-    next_available_actions: torch.tensor = None
-    next_available_actions_mask: torch.tensor = None
+    next_available_actions: Optional[torch.tensor] = None
+    next_available_actions_mask: Optional[torch.tensor] = None
     done: torch.tensor = None
     weight: torch.tensor = None
