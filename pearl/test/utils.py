361aac496cadd6936691a03c136171a9e8e3f157
Jalaj Bhandari <jalajbhandari@meta.com>
2023-06-26 15:03:39 -0700

    A unit test for vanilla mlps
    Summary:
    What: This is a simple test to check if vanilla mlps are able to learn a custom function.
    
    Why: The need for a unit test to check instantiation of mlps, and their learnability was felt.
    
    How: the training data is generated with x sampled from a multivariate normal distribution and y as the corresponding pdf values. A vanilla mlp is trained to learn this mapping. The training loss (mse error) should go down with training steps.
    
    Differential Revision: D47030273
    
    fbshipit-source-id: d5abfb8142b9153d6e3f61160b749365df1a6a6a

--- pearl/test/utils.py
+++ pearl/test/utils.py
@@ -59,3 +59,28 @@ def create_random_batch(
         done=done,
     )
     return batch, action_space
+
+
+# for testing vanilla mlps
+def create_normal_pdf_training_data(
+    input_dim: int, num_data_points: int
+) -> (torch.Tensor, torch.Tensor):
+
+    """
+    x are sampled from a multivariate normal distribution
+    y are the corresponding pdf values
+    """
+    mean = torch.zeros(input_dim)
+    sigma = torch.eye(input_dim)
+
+    multi_variate_normal = torch.distributions.MultivariateNormal(mean, sigma)
+    x = multi_variate_normal.sample(
+        (num_data_points,)
+    )  # sample from a mvn distribution
+    y = torch.exp(
+        -0.5 * ((x - mean) @ torch.inverse(sigma) * (x - mean)).sum(dim=1)
+    ) / (
+        torch.sqrt((2 * torch.tensor(3.14)) ** mean.shape[0] * torch.det(sigma))
+    )  # corresponding pdf of mvn
+    y_corrupted = y + 0.01 * torch.randn(num_data_points)  # noise corrupted targets
+    return x, y_corrupted
