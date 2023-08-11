497d195d239ce4ba2d153d0eb283f7515fceace8
Liyuan Wang <wangliyuan316@meta.com>
2023-08-11 15:56:08 -0700

    split HER from existing cogwheel tests
    Summary:
    Why:
    HER test takes long, also it is not a new policy learner, but a new replay buffer.
    
    How:
    split it from existing cogwheel test
    
    Reviewed By: rodrigodesalvobraz
    
    Differential Revision: D48279005
    
    fbshipit-source-id: 339602b67e319a20b201b649deb8706b3ee39949

---
+++ pearl/test/integration/integration_replay_buffer_tests.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env fbpython
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+import unittest
+
+import torch
+
+from pearl.core.common.pearl_agent import PearlAgent
+
+from pearl.core.common.replay_buffer.hindsight_experience_replay_buffer import (
+    HindsightExperienceReplayBuffer,
+)
+from pearl.core.sequential_decision_making.policy_learners.deep_q_learning import (
+    DeepQLearning,
+)
+
+from pearl.online_learning.online_learning import (
+    episode_return,
+    target_return_is_reached,
+)
+from pearl.utils.sparse_reward_environment import (
+    DiscreteSparseRewardEnvironment,
+    SparseRewardEnvSummarizationModule,
+)
+
+
+class IntegrationReplayBufferTests(unittest.TestCase):
+    """
+    Integration test for replay buffer
+    """
+
+    def test_her(self) -> None:
+        """
+        This test is to ensure HER works for sparse reward environment
+                DQN is not able to solve this problem within 1000 episodes
+        """
+        env = DiscreteSparseRewardEnvironment(
+            length=50,
+            height=50,
+            step_size=1,
+            action_count=8,
+            max_episode_duration=1000,
+            reward_distance=1,
+        )
+
+        def done_fn(state, action):
+            next_state = state[:2] + torch.Tensor(env._actions[action])
+            goal = state[-2:]
+            if torch.all(torch.eq(next_state, goal)):
+                return True
+            return False
+
+        def reward_fn(state, action):
+            done = done_fn(state, action)
+            if done:
+                return 0
+            return -1
+
+        agent = PearlAgent(
+            policy_learner=DeepQLearning(
+                4,
+                env.action_space,
+                [64, 64],
+                training_rounds=10,
+                batch_size=500,
+            ),
+            replay_buffer=HindsightExperienceReplayBuffer(
+                2_000_000, goal_dim=2, reward_fn=reward_fn, done_fn=done_fn
+            ),
+            history_summarization_module=SparseRewardEnvSummarizationModule(),
+        )
+        # precollect data
+        for _ in range(500):
+            episode_return(agent, env, False, False, False)
+
+        self.assertTrue(
+            target_return_is_reached(
+                agent=agent,
+                env=env,
+                # -600 and 1000 are good enough to differ perf from DQN
+                # set it looser for HER to make test faster
+                target_return=-600,
+                max_episodes=1000,
+                learn=True,
+                learn_after_episode=True,
+                exploit=False,
+                check_moving_average=True,
+            )
+        )
--- pearl/test/integration/integration_tests.py
+++ pearl/test/integration/integration_tests.py
@@ -19,9 +19,6 @@ from pearl.core.common.replay_buffer.fifo_off_policy_replay_buffer import (
 from pearl.core.common.replay_buffer.fifo_on_policy_replay_buffer import (
     FIFOOnPolicyReplayBuffer,
 )
-from pearl.core.common.replay_buffer.hindsight_experience_replay_buffer import (
-    HindsightExperienceReplayBuffer,
-)
 from pearl.core.common.replay_buffer.on_policy_episodic_replay_buffer import (
     OnPolicyEpisodicReplayBuffer,
 )
@@ -52,10 +49,6 @@ from pearl.online_learning.online_learning import (
     target_return_is_reached,
 )
 from pearl.utils.environments import OneHotObservationsFromDiscrete
-from pearl.utils.sparse_reward_environment import (
-    DiscreteSparseRewardEnvironment,
-    SparseRewardEnvSummarizationModule,
-)
 
 
 class IntegrationTests(unittest.TestCase):
@@ -314,65 +307,6 @@ class IntegrationTests(unittest.TestCase):
                 learn=True,
                 learn_after_episode=True,
                 exploit=False,
-            )
-        )
-
-    def test_her(self) -> None:
-        """
-        This test is to ensure HER works for sparse reward environment
-                DQN is not able to solve this problem within 1000 episodes
-        """
-        env = DiscreteSparseRewardEnvironment(
-            length=50,
-            height=50,
-            step_size=1,
-            action_count=8,
-            max_episode_duration=1000,
-            reward_distance=1,
-        )
-
-        def done_fn(state, action):
-            next_state = state[:2] + torch.Tensor(env._actions[action])
-            goal = state[-2:]
-            if torch.all(torch.eq(next_state, goal)):
-                return True
-            return False
-
-        def reward_fn(state, action):
-            done = done_fn(state, action)
-            if done:
-                return 0
-            return -1
-
-        agent = PearlAgent(
-            policy_learner=DeepQLearning(
-                4,
-                env.action_space,
-                [64, 64],
-                training_rounds=10,
-                batch_size=500,
-            ),
-            replay_buffer=HindsightExperienceReplayBuffer(
-                2_000_000, goal_dim=2, reward_fn=reward_fn, done_fn=done_fn
-            ),
-            history_summarization_module=SparseRewardEnvSummarizationModule(),
-        )
-        # precollect data
-        for _ in range(500):
-            episode_return(agent, env, False, False, False)
-
-        self.assertTrue(
-            target_return_is_reached(
-                agent=agent,
-                env=env,
-                # -600 and 1000 are good enough to differ perf from DQN
-                # set it looser for HER to make test faster
-                target_return=-600,
-                max_episodes=1000,
-                learn=True,
-                learn_after_episode=True,
-                exploit=False,
-                check_moving_average=True,
             )
         )
 
