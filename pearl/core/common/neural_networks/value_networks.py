af73bc76b428817f8874f190688d59da81025f33
Jalaj Bhandari <jalajbhandari@meta.com>
2023-08-15 10:35:46 -0700

    base cnn class
    Summary:
    base cnn class with convolutional layers along with minor changes to base mlp class.
    
    A reference to open source vision datasets which are torch based: https://pytorch.org/vision/0.8/datasets.html
    
    Reviewed By: rodrigodesalvobraz
    
    Differential Revision: D48303909
    
    fbshipit-source-id: 284e79506c573f6b4e5357ff068ae75bb09d124c

--- pearl/core/common/neural_networks/value_networks.py
+++ pearl/core/common/neural_networks/value_networks.py
@@ -18,21 +18,88 @@ from pearl.utils.extend_state_feature import (
 from torch import Tensor
 
 
+def mlp_block(
+    input_dim: int,
+    hidden_dims: Optional[List[int]],
+    output_dim: int = 1,
+    use_batch_norm: bool = False,
+) -> nn.Module:
+    """
+    A simple MLP which can be reused to create more complex networks
+    Args:
+        input_dim: dimension of the input layer
+        hidden_dims: a list of dimensions of the hidden layers
+        output_dim: dimension of the output layer
+        use_batch_norm: whether to use batch_norm or not in the hidden layers
+    Returns:
+        an nn.Sequential module consisting of mlp layers
+    """
+    dims = [input_dim] + hidden_dims + [output_dim]
+    layers = []
+    for i in range(len(dims) - 2):
+        layers.append(nn.Linear(dims[i], dims[i + 1]))
+        if use_batch_norm:
+            layers.append(nn.BatchNorm1d(dims[i + 1]))
+        layers.append(nn.ReLU())
+
+    layers.append(nn.Linear(dims[-2], dims[-1]))
+    return nn.Sequential(*layers)
+
+
+def conv_block(
+    input_channels_count: int,
+    output_channels_list: List[int],
+    kernel_sizes: List[int],
+    strides: List[int],
+    paddings: List[int],
+    use_batch_norm=False,
+) -> nn.Module:
+    """
+    Reminder: torch.Conv2d layers expect inputs as (batch_size, in_channels, height, width)
+    Notes: layer norm is typically not used with CNNs
+
+    Args:
+        input_channels_count: number of input channels
+        output_channels_list: a list of number of output channels for each convolutional layer
+        kernel_sizes: a list of kernel sizes for each layer
+        strides: a list of strides for each layer
+        paddings: a list of paddings for each layer
+        use_batch_norm: whether to use batch_norm or not in the convolutional layers
+    Returns:
+        an nn.Sequential module consisting of convolutional layers
+    """
+    layers = []
+    for out_channels, kernel_size, stride, padding in zip(
+        output_channels_list, kernel_sizes, strides, paddings
+    ):
+        conv_layer = nn.Conv2d(
+            input_channels_count,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+        layers.append(conv_layer)
+        if use_batch_norm and input_channels_count > 1:
+            layers.append(
+                nn.BatchNorm2d(input_channels_count)
+            )  # input to Batchnorm 2d is the number of input channels
+        layers.append(nn.ReLU())
+        input_channels_count = out_channels  # number of input channels to next layer is number of output channels of previous layer
+
+    return nn.Sequential(*layers)
+
+
 class VanillaValueNetwork(AutoDeviceNNModule):
     def __init__(
-        self, input_dim: int, hidden_dims: Optional[List[int]], output_dim: int = 1
+        self,
+        input_dim: int,
+        hidden_dims: Optional[List[int]],
+        output_dim: int = 1,
     ) -> None:
 
         super(VanillaValueNetwork, self).__init__()
-
-        self._dims = [input_dim] + hidden_dims + [output_dim]
-        self._layers = []
-        for i in range(len(self._dims) - 2):
-            self._layers.append(nn.Linear(self._dims[i], self._dims[i + 1]))
-            self._layers.append(nn.ReLU())
-
-        self._layers.append(nn.Linear(self._dims[-2], self._dims[-1]))
-        self._model = nn.Sequential(*self._layers)
+        self._model = mlp_block(input_dim, hidden_dims, output_dim)
 
     def forward(self, x: Tensor) -> Tensor:
         return self._model(x)
@@ -42,6 +109,99 @@ class VanillaValueNetwork(AutoDeviceNNModule):
         for layer in self._model:
             if isinstance(layer, nn.Linear):
                 nn.init.xavier_normal_(layer.weight)
+
+
+class VanillaCNN(AutoDeviceNNModule):
+    """
+    Vanilla CNN with a convolutional block followed by an mlp block.
+    Args:
+        input_width: width of the input
+        input_height: height of the input
+        input_channels_count: number of input channels
+        kernel_sizes: list of kernel sizes for the convolutional layers
+        output_channels_list: list of number of output channels for each convolutional layer
+        strides: list of strides for each layer
+        paddings: list of paddings for each layer
+        hidden_dims_fully_connected: a list of dimensions of the hidden layers in the mlp
+        use_batch_norm_conv: whether to use batch_norm in the convolutional layers
+        use_batch_norm_fully_connected: whether to use batch_norm in the fully connected layers
+        output_dim: dimension of the output layer
+    Returns:
+        An nn.Sequential module consisting of a convolutional block followed by an mlp.
+    """
+
+    def __init__(
+        self,
+        input_width: int,
+        input_height: int,
+        input_channels_count: int,
+        kernel_sizes: List[int],
+        output_channels_list: List[int],
+        strides: List[int],
+        paddings: List[int],
+        hidden_dims_fully_connected: Optional[
+            List[int]
+        ] = None,  # hidden dims for fully connected layers
+        use_batch_norm_conv: bool = False,
+        use_batch_norm_fully_connected: bool = False,
+        output_dim: int = 1,  # dimension of the final output
+    ):
+
+        assert (
+            len(kernel_sizes)
+            == len(output_channels_list)
+            == len(strides)
+            == len(paddings)
+        )
+        super(VanillaCNN, self).__init__()
+
+        self._input_channels = input_channels_count
+        self._input_height = input_height
+        self._input_width = input_width
+        self._output_channels = output_channels_list
+        self._kernel_sizes = kernel_sizes
+        self._strides = strides
+        self._paddings = paddings
+        if hidden_dims_fully_connected is None:
+            self._hidden_dims_fully_connected = []
+        else:
+            self._hidden_dims_fully_connected = hidden_dims_fully_connected
+
+        self._use_batch_norm_conv = use_batch_norm_conv
+        self._use_batch_norm_fully_connected = use_batch_norm_fully_connected
+        self._output_dim = output_dim
+
+        self._model_cnn = conv_block(
+            input_channels_count=self._input_channels,
+            output_channels_list=self._output_channels,
+            kernel_sizes=self._kernel_sizes,
+            strides=self._strides,
+            paddings=self._paddings,
+            use_batch_norm=self._use_batch_norm_conv,
+        )
+
+        self._mlp_input_dims = self.compute_output_dim_model_cnn()
+        self._model_fc = mlp_block(
+            input_dim=self._mlp_input_dims,
+            hidden_dims=self._hidden_dims_fully_connected,
+            output_dim=self._output_dim,
+            use_batch_norm=self._use_batch_norm_fully_connected,
+        )
+
+    def compute_output_dim_model_cnn(self) -> int:
+        dummy_input = torch.zeros(
+            1, self._input_channels, self._input_width, self._input_height
+        )
+        dummy_output_flattened = torch.flatten(
+            self._model_cnn(dummy_input), start_dim=1, end_dim=-1
+        )
+        return dummy_output_flattened.shape[1]
+
+    def forward(self, x: Tensor) -> Tensor:
+        out_cnn = self._model_cnn(x)
+        out_flattened = torch.flatten(out_cnn, start_dim=1, end_dim=-1)
+        out_fc = self._model_fc(out_flattened)
+        return out_fc
 
 
 class VanillaStateActionValueNetwork(VanillaValueNetwork):
---
+++ pearl/test/unit/with_pytorch/test_vanilla_cnns.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env fbpython
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+import unittest
+
+import torch
+import torchvision
+from pearl.core.common.neural_networks.value_networks import VanillaCNN
+
+from torch import optim
+from torch.utils.data import DataLoader, Subset
+from torchvision import transforms
+
+
+class TestVanillaMlps(unittest.TestCase):
+    def setUp(self) -> None:
+        transform = transforms.Compose([transforms.ToTensor()])
+        mnist_dataset = torchvision.datasets.MNIST(
+            root="./", train=True, transform=transform, download=True
+        )
+
+        self.num_data_points = 5000
+        self.mnist_train_dataset = Subset(
+            mnist_dataset, torch.arange(1, self.num_data_points)
+        )
+        self.learning_rate = 0.001
+        self.batch_size = 64
+        self.num_epochs = 10
+
+        self.train_dl = DataLoader(
+            self.mnist_train_dataset, self.batch_size, shuffle=True
+        )
+
+    def test_vanilla_cnns(self) -> None:
+        """
+        a simple cnn should be able to fit the mnist digit dataset and the training
+        accuracy should be close to 90%
+        """
+        input_width = 28  # specifications for mnist dataset
+        input_height = 28  # specifications for mnist dataset
+        input_channels = 1  # specifications for mnist dataset
+
+        # build a simple cnn
+        kernel_sizes = [5]
+        output_channels = [16]
+        strides = [1]
+        paddings = [2]
+        hidden_dims_fully_connected = [64]
+        output_dim = 10
+        network = VanillaCNN(
+            input_width=input_width,
+            input_height=input_height,
+            input_channels_count=input_channels,
+            kernel_sizes=kernel_sizes,
+            output_channels_list=output_channels,
+            strides=strides,
+            paddings=paddings,
+            hidden_dims_fully_connected=hidden_dims_fully_connected,
+            use_batch_norm_conv=True,  # testing to see if batch normalization works
+            use_batch_norm_fully_connected=True,  # testing to see if batch normalization works
+            output_dim=output_dim,
+        )
+
+        optimizer = optim.AdamW(
+            network.parameters(), lr=self.learning_rate, amsgrad=True
+        )
+        criterion = torch.nn.CrossEntropyLoss()
+        accuracy_train = 0
+
+        # training loop
+        accuracy_above_threshold = False
+        for _ in range(self.num_epochs):
+            # one epoch loops over entire dataset
+            for x_batch, y_batch in self.train_dl:
+                pred = network(x_batch)  # generate predictions
+                loss = criterion(pred, y_batch)  # crossentropy loss for classification
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                is_correct = (torch.argmax(pred, dim=1) == y_batch).float()
+                accuracy_train += is_correct.sum()
+
+            accuracy_train /= len(self.train_dl.dataset)
+
+            if accuracy_train > 0.9:
+                accuracy_above_threshold = True
+                break
+
+        self.assertTrue(
+            accuracy_above_threshold
+        )  # training accuracy should reach above 0.9 with learning steps
