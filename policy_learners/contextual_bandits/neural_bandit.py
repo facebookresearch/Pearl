3795ad2149408f1cf3b53d1fa3cba3b883bbc5be
Liyuan Wang <wangliyuan316@meta.com>
2023-09-15 15:51:03 -0700

    remove ddp wrapper inside policy learner
    Summary: Along with proposal from Zhenyu earlier this week, if we need ddp wrapper, it should be done outside of policy learner, as policy learner is an nn.Module now
    
    Reviewed By: alexnikulkov
    
    Differential Revision: D49336220
    
    fbshipit-source-id: 6eee92505c4a9be82a4161d8ad827865a1cc81f0

--- policy_learners/contextual_bandits/neural_bandit.py
+++ policy_learners/contextual_bandits/neural_bandit.py
@@ -23,7 +23,6 @@ from pearl.policy_learners.exploration_modules.exploration_module import (
 from pearl.replay_buffers.transition import TransitionBatch
 from pearl.utils.instantiations.action_spaces.action_spaces import DiscreteActionSpace
 from torch import optim
-from torch.nn.parallel import DistributedDataParallel
 
 
 class NeuralBandit(ContextualBanditBase):
@@ -56,12 +55,6 @@ class NeuralBandit(ContextualBanditBase):
             output_dim=output_dim,
             **kwargs,
         )
-        if self.distribution_enabled:
-            self._deep_represent_layers.to(self.device)
-            self._deep_represent_layers = DistributedDataParallel(
-                self._deep_represent_layers,
-                device_ids=None if self.device.type == "cpu" else [self.device],
-            )
         self._optimizer = optim.AdamW(
             self._deep_represent_layers.parameters(), lr=learning_rate, amsgrad=True
         )
