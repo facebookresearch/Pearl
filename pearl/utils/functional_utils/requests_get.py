19c06317097a99a4454bb8062072e05025432e72
frankchengmeta <frankcheng@meta.com>
2024-06-11 03:37:59 -0700

    Update requests_get.py (#95)
    Summary: Pull Request resolved: https://github.com/facebookresearch/Pearl/pull/95
    
    Reviewed By: rodrigodesalvobraz
    
    Differential Revision: D58262688
    
    Pulled By: frankchengmeta
    
    fbshipit-source-id: ae2b086b679942ed8d0fdb09fac75c8600583e4e

--- pearl/utils/functional_utils/requests_get.py
+++ pearl/utils/functional_utils/requests_get.py
@@ -1,4 +1,10 @@
 # pyre-strict
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import requests
 
 
