171a5ad6af4ce0ebeb4782494d1ce338168873fe
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2025-06-25 23:06:35 +0530

    patch: test support

--- test/__init__.py
+++ test/__init__.py
@@ -4,13 +4,4 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
-import pkgutil
-
-__path__ = pkgutil.extend_path(__path__, __name__)
-
-ROOT_TEST_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "test"
-)
-if os.path.isdir(ROOT_TEST_DIR) and ROOT_TEST_DIR not in __path__:
-    __path__.append(ROOT_TEST_DIR)
+# pyre-strict
---
+++ test/conftest.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+# pyre-strict
+
+import os
+import sys
+
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
