a46c23bf79033e267e6f003de1d2613462d9a16b
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2025-06-25 22:27:56 +0530

    patch: test support

--- test/__init__.py
+++ test/__init__.py
@@ -3,3 +3,11 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+from pkgutil import extend_path
+from pathlib import Path
+
+ROOT_TEST_DIR = Path(__file__).resolve().parents[2] / "test"
+__path__ = list(extend_path(__path__, __name__))
+if str(ROOT_TEST_DIR) not in __path__:
+    __path__.append(str(ROOT_TEST_DIR))
