677162e8bffc1f271e910409df6f71a531e2cb49
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2025-06-25 23:09:11 +0530

    patch: test support

--- test/conftest.py
+++ test/conftest.py
@@ -10,8 +10,6 @@
 import os
 import sys
 
-__path__ = pkgutil.extend_path(__path__, __name__)
-
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
