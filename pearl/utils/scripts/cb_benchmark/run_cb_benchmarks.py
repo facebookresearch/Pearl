07e7a2a91355e379e596b91c981564b924f60a29
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-04-16 03:27:08 -0700

    Fix Unused Import issue in fbcode/pearl/utils/scripts/cb_benchmark/run_cb_benchmarks.py
    Differential Revision: D101135614
    
    fbshipit-source-id: 8e6e289c93ba2945665b8beb0bd0a3dd5fb814e7

--- pearl/utils/scripts/cb_benchmark/run_cb_benchmarks.py
+++ pearl/utils/scripts/cb_benchmark/run_cb_benchmarks.py
@@ -9,7 +9,7 @@
 
 import os
 import random
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import pandas as pd
 from pearl.action_representation_modules.action_representation_module import (
