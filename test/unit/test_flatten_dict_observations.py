a5791e3ee8709169d3ab9de44fb011697962e952
generatedunixname1228101065403401 <noreply+1228101065403401@fb.com>
2026-05-03 03:11:31 -0700

    Fix PYRE_FIXME issue in fbcode/pearl/test/unit/test_flatten_dict_observations.py
    Differential Revision: D103580521
    
    fbshipit-source-id: 5b1396a22defbb9f179bfee5d420d013e2b3eafc

--- test/unit/test_flatten_dict_observations.py
+++ test/unit/test_flatten_dict_observations.py
@@ -31,8 +31,7 @@ class DummyDictEnv(Environment):
         self._observation_space = gym.spaces.Dict(
             # pyrefly: ignore [bad-argument-type]
             {
-                # pyre-fixme[6]: gym stubs don't recognize np.float32 as valid dtype
-                "x": gym.spaces.Box(low=0, high=1, shape=(2,), dtype=np.float32),
+                "x": gym.spaces.Box(low=0, high=1, shape=(2,)),
                 "y": gym.spaces.Discrete(3),
             }
         )
