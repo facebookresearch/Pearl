c7050abe7aa8b743744d2e7bde60bca19debeb9d
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2026-08-17 12:19:04 -0700

    Remove Pyre mode headers
    Reviewed By: maggiemoss
    
    Differential Revision: D116281750
    
    fbshipit-source-id: 3f717191644a9f999fdbe845e306f01195a9e358

--- pearl/action_representation_modules/__init__.py
+++ pearl/action_representation_modules/__init__.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-strict
 
 from .action_representation_module import ActionRepresentationModule
 from .binary_action_representation_module import BinaryActionTensorRepresentationModule
